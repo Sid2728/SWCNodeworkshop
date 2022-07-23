7a8852a75d505e674e5f36185eab25711bebb2f8
Siddhant <103687316+Sid2728@users.noreply.github.com>
2022-07-24 00:34:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -138,6 +138,6 @@ app.post("/edit/:id", async(req,res)=>{
 
 
 
-app.listen(3000 || process.env.PORT , function () {
+app.listen(process.env.PORT || 3000 , function () {
   console.log("Server started on port 3000");
 });
